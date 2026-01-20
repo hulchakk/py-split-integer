6d03e14121640a9f2ae24bcc6c94bad77053c4a6
Dmytro <ys33ys33ys55@gmail.com>
2026-01-20 12:24:18 +0100

    Solution fixed

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,26 +2,20 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    result = split_integer(17, 4)
-    assert sum(result) == 17
+    assert sum(split_integer(17, 4)) == 17
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    result = split_integer(16, 4)
-    for part in result:
-        assert part == 4
+    assert split_integer(16, 4) == [4, 4, 4, 4]
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    result = split_integer(100, 1)
-    assert result[-1] == 100 and result[0] == 100
+    assert split_integer(100, 1) == [100]
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    result = split_integer(32, 6)
-    assert result == sorted(result)
+    assert split_integer(32, 6) == [5, 5, 5, 5, 6, 6]
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    result = split_integer(1, 10)
-    assert result[-1] == 1 and len(result) == 10
+    assert split_integer(1, 10) == [0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
